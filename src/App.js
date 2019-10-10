f11663ce64688074223044efcbe81cf3e057e2e8
Crawford <David.Crawford3@jacobs.com>
2019-10-10 17:46:45 -0400

    many changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import backgroundVideo from "./content/video/militaryConstruction.mp4"
+
+var apiURI = "http://localhost:9000/testAPI/"
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state =  { apiResponse: "" };
+    this.state =  { 
+      "logo-text": "",
+      "taglink-text": "",
+      "tagline-text": "" 
+    };
   }
 
   callAPI() {
-    fetch("http://localhost:9000/testAPI")
-      .then(res => res.text())
-      .then(res => this.setState({ apiResponse: res }))
+    fetch(apiURI)
+      .then(res => res.json())
+      .then(res => this.setState({ 
+        "logo-text": res["logo-text"],
+         "taglink-text": res["taglink-text"],
+         "tagline-text": res["tagline-text"] 
+      }))
+      .then(function (res) {
+        console.log(res.text())
+      })
       .catch(err => err);
   }
 
@@ -19,14 +33,65 @@ class App extends Component {
     this.callAPI();
   }
 
+  getImage(apiId){
+    var uri = apiURI + apiId
+    fetch(uri)
+      .then(res => res.text())
+      .catch(err => err);
+  }
+
   render() {
+    var divStyle = {
+      color: 'white',
+      position: 'absolute',
+      top: "10px",
+      fontSize: "15px"
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.state.apiResponse}</h1>
+      <div>
+
+        <header>
+
+          <div className="overlay"></div>
+          <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+            <source src={backgroundVideo} type="video/mp4"></source>
+          </video>
+          <div className="container h-100">
+
+          {/*<ul class="nav">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+    </ul>*/}
+
+          <nav class="navbar navbar-light">
+            <a class="navbar-brand" style={divStyle} href="#">{this.state["logo-text"]}</a>
+          </nav>
+
+            <div className="d-flex h-100 text-center align-items-center">
+              <div className="w-100 text-white">
+                <h1 className="helveticaBoldCond">{this.state["taglink-text"]}</h1>
+              </div>
+            </div>
+          </div>
         </header>
-        {/*<p className="App-intro">{this.state.apiResponse}</p>*/}
+
+
+            <div className="row blueBox">
+              <div className="col-md-9 mx-auto ">
+                <p className="paragraphStyle">{this.state["tagline-text"]}</p>
+              </div>
+            </div>
+
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './scripts/css/backgroundVideo.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
